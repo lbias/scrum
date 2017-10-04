4095a20be42f8670f7bee6d72bf378f5232a67ba
lbias <306508354@qq.com>
2017-10-04 17:00:44 +0800

    add search fields and ordering fields to each viewset

--- board/views.py
+++ board/views.py
@@ -32,12 +32,16 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+    search_fields = ('name', )
+    ordering_fields = ('end', 'name', )
 
 class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
     """API endpoint for listing and creating tasks."""
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    search_fields = ('name', 'description', )
+    ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
 class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     """API endpoint for listing users."""
@@ -46,3 +50,4 @@ class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
     lookup_url_kwarg = User.USERNAME_FIELD
     queryset = User.objects.order_by(User.USERNAME_FIELD)
     serializer_class = UserSerializer
+    search_fields = (User.USERNAME_FIELD, )
