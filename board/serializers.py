e9c6461f185416d319ca3a0cb9a6b7b846f5900c
lbias <306508354@qq.com>
2017-10-04 15:13:09 +0800

    create serializer for model user

--- board/serializers.py
+++ board/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import serializers
 
 from .models import Sprint, Task
@@ -18,3 +20,10 @@ class TaskSerializer(serializers.ModelSerializer):
 
     def get_status_display(self, obj):
         return obj.get_status_display()
+
+class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.CharField(source='get_full_name', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ('id', User.USERNAME_FIELD, 'full_name', 'is_active', )
