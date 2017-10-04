1de8c024375ffa7886bebf12a810a98639a3088e
lbias <306508354@qq.com>
2017-10-04 18:08:49 +0800

    return task that isn't assigned to a sprint

--- board/forms.py
+++ board/forms.py
@@ -2,7 +2,17 @@ import django_filters
 
 from .models import Task
 
+class NullFilter(django_filters.BooleanFilter):
+    """Filter on a field set as null or not."""
+
+    def filter(self, qs, value):
+        if value is not None:
+            return qs.filter(**{'%s__isnull' % self.name: value})
+            return qs
+
 class TaskFilter(django_filters.FilterSet):
+    backlog = NullFilter(name='sprint')
+
     class Meta:
         model = Task
-        fields = ('sprint', 'status', 'assigned', )
+        fields = ('sprint', 'status', 'assigned', 'backlog', )
