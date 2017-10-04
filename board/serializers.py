b8cbae297da6d3fdfaea72abc17bbce56c3711ea
lbias <306508354@qq.com>
2017-10-04 15:01:47 +0800

    status_display to show the text associated with its state

--- board/serializers.py
+++ board/serializers.py
@@ -8,7 +8,11 @@ class SprintSerializer(serializers.ModelSerializer):
         fields = ('id', 'name', 'description', 'end', )
 
 class TaskSerializer(serializers.ModelSerializer):
+    status_display = serializers.SerializerMethodField('get_status_display')
     class Meta:
         model = Task
-        fields = ('id', 'name', 'description', 'sprint', 'status', 'order',
+        fields = ('id', 'name', 'description', 'sprint', 'status', 'status_display', 'order',
                   'assigned', 'started', 'due', 'completed', )
+
+    def get_status_display(self, obj):
+        return obj.get_status_display()
