83aed37240583adf072b05cbdf17362af4325d2a
lbias <306508354@qq.com>
2017-10-04 15:19:05 +0800

    create viewsets for tasks and users

--- board/views.py
+++ board/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth import get_user_model
+
 from rest_framework import authentication, permissions, viewsets
 
-from .models import Sprint
-from .serializers import SprintSerializer
+from .models import Sprint, Task
+from .serializers import SprintSerializer, TaskSerializer, UserSerializer
+
+User = get_user_model()
 
 class DefaultsMixin(object):
     """Default settings for view authentication, permissions,
@@ -22,3 +26,17 @@ class SprintViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Sprint.objects.order_by('end')
     serializer_class = SprintSerializer
+
+class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
+    """API endpoint for listing and creating tasks."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+class UserViewSet(DefaultsMixin, viewsets.ReadOnlyModelViewSet):
+    """API endpoint for listing users."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
