f6df5ad9721d64aadf9712a8dc818468b1646b59
lbias <306508354@qq.com>
2017-10-04 17:10:28 +0800

    associate taskfilter with the taskviewset in board/views

--- board/views.py
+++ board/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 
 from rest_framework import authentication, permissions, viewsets, filters
 
+from .forms import TaskFilter
 from .models import Sprint, Task
 from .serializers import SprintSerializer, TaskSerializer, UserSerializer
 
@@ -40,6 +41,7 @@ class TaskViewSet(DefaultsMixin, viewsets.ModelViewSet):
 
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filter_class = TaskFilter
     search_fields = ('name', 'description', )
     ordering_fields = ('name', 'order', 'started', 'due', 'completed', )
 
