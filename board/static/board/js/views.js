190051bc0434fab795a03e35219bc284b20e385f
lbias <306508354@qq.com>
2017-10-06 23:03:47 +0800

    refactor with TemplateView, add HomepageView, LoginView to app.views dictionary

--- board/static/board/js/views.js
+++ board/static/board/js/views.js
@@ -15,35 +15,15 @@
     }
   });
 
-  var HomepageView = Backbone.View.extend({
-    templateName: '#home-template',
-    initialize: function () {
-      this.template = _.template($(this.templateName).html()); },
-    render: function () {
-      var context = this.getContext(),
-        html = this.template(context);
-      this.$el.html(html);
-    },
-    getContext: function () {
-      return {};
-    }
+  var HomepageView = TemplateView.extend({
+    templateName: '#home-template'
   });
 
-  var LoginView =  Backbone.View.extend({
+  var LoginView = TemplateView.extend({
     id: 'login',
     templateName: '#login-template',
-    initialize: function () {
-      this.template = _.template($(this.templateName).html());
-    },
-    render: function () {
-      var context = this.getContext(),
-        html = this.template(context);
-      this.$el.html(html);
-    },
-    getContext: function () {
-      return {};
-    }
   });
 
   app.views.HomepageView = HomepageView;
+  app.views.LoginView = LoginView;
 })(jQuery, Backbone, _, app);
