bdaa6ff7ee0b1644c474053b13b53f09a4c8d897
lbias <306508354@qq.com>
2017-10-08 20:28:52 +0800

    create FormView

--- board/static/board/js/views.js
+++ board/static/board/js/views.js
@@ -15,6 +15,51 @@
     }
   });
 
+  var FormView = TemplateView.extend({
+      events: {
+          'submit form': 'submit'
+      },
+      errorTemplate: _.template('<span class="error"><%- msg %></span>'),
+      clearErrors: function () {
+          $('.error', this.form).remove();
+      },
+      showErrors: function (errors) {
+          _.map(errors, function (fieldErrors, name) {
+              var field = $(':input[name=' + name + ']', this.form),
+                  label = $('label[for=' + field.attr('id') + ']', this.form);
+              if (label.length === 0) {
+                  label = $('label', this.form).first();
+              }
+              function appendError(msg) {
+                  label.before(this.errorTemplate({msg: msg}));
+              }
+              _.map(fieldErrors, appendError, this);
+          }, this);
+      },
+      serializeForm: function (form) {
+          return _.object(_.map(form.serializeArray(), function (item) {
+              // Convert object to tuple of (name, value)
+              return [item.name, item.value];
+          }));
+      },
+      submit: function (event) {
+          event.preventDefault();
+          this.form = $(event.currentTarget);
+          this.clearErrors();
+      },
+      failure: function (xhr, status, error) {
+          var errors = xhr.responseJSON;
+          this.showErrors(errors);
+      },
+      done: function (event) {
+          if (event) {
+              event.preventDefault();
+          }
+          this.trigger('done');
+          this.remove();
+      }
+  });
+
   var HomepageView = TemplateView.extend({
     templateName: '#home-template'
   });
